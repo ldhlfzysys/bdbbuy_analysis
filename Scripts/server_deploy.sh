b016319942bea118c1283d305370b122b029a5e6
weixi1 <weixi1@staff.weibo.com>
2019-01-04 00:16:10 +0800

    部署的shell bug修复

--- Scripts/server_deploy.sh
+++ Scripts/server_deploy.sh
@@ -5,4 +5,4 @@ git pull
 sudo fuser -k 8000/tcp
 
 cd /root/Bdbproject/bdbbuy_analysis/BdbbuyDataCenter
-nohup python manage.py runserver_plus --cert-file server.cr--key-file server_nopwd.key   0.0.0.0:8000 >/dev/null 2>&1 &
+nohup python manage.py runserver_plus --cert-file server.crt --key-file server_nopwd.key  0.0.0.0:8000 >/dev/null 2>&1 &
